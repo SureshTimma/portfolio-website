960709e45a49430c4c7dd3bb93dc0ced8cc70004
SureshTimma <148480722+SureshTimma@users.noreply.github.com>
2025-04-06 00:46:09 +0530

    about section updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Suresh Timma",
-  profile: '/profile.png',
+  profile: '/portfolio.jpg',
   designation: "Software Developer",
   description: "My name is Suresh Timma. I am a first-year student at NIAT with a passion for AI, Deep Learning, and Full-Stack Development. I am a quick learner with a strong self-learning attitude. I love exploring new technologies and solving real-world problems through code. I have experience with Python, JavaScript, HTML, CSS, and the MERN stack. I am also the Co-founder of Abigna Academy, a platform focused on accessible education. I am open to any opportunity that aligns with my skills and interests.",
   email: 'work.shtim@gmail.com',
