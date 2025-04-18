205ca198de0cb8cdd238deedef005dc084d587d7
SureshTimma <148480722+SureshTimma@users.noreply.github.com>
2025-04-19 01:02:04 +0530

    social icons changed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,16 +4,16 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is Suresh Timma. I am a first-year student at NIAT with a passion for AI, Deep Learning, and Full-Stack Development. I am a quick learner with a strong self-learning attitude. I love exploring new technologies and solving real-world problems through code. I have experience with Python, JavaScript, HTML, CSS, and the MERN stack. I am also the Co-founder of Abigna Academy, a platform focused on accessible education. I am open to any opportunity that aligns with my skills and interests.",
   email: 'work.shtim@gmail.com',
-  phone: '+91 9876543210',
-  address: 'Financial District, Hyderabad, Telangana, India - 500032',
+  phone: '+91 7207409009',
+  address: 'Kapil Kavuri Hub, Financial District, Hyderabad, Telangana, India - 500032',
   github: 'https://github.com/SureshTimma',
-  facebook: 'https://www.facebook.com/sureshtimma/',
+  facebook: '#',
   linkedIn: 'https://www.linkedin.com/in/sureshtimma/',
-  twitter: 'https://twitter.com/sureshtimma',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/suresh-timma',
+  twitter: '#',
+  stackOverflow: '#',
   leetcode: "https://leetcode.com/u/SureshTimma/",
   devUsername: "",
   youtube: "https://www.youtube.com/@suresh_timma",
   instagram: "https://www.instagram.com/suresh_timma/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "#"
 }
